783fa26acaa7be074ca0567601507cd11f1341a6
dkrupkina <dkrupkina@TeamCity>
2023-07-07 09:49:54 +0000

    TeamCity change in 'Java Maven Demo' project: Updated cloud profile 'amazon-1' in project 'project2'

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.amazonEC2CloudImage
 import jetbrains.buildServer.configs.kotlin.amazonEC2CloudProfile
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
@@ -33,6 +34,16 @@ project {
     buildType(Build)
 
     features {
+        amazonEC2CloudImage {
+            id = "PROJECT_EXT_3"
+            profileId = "amazon-1"
+            name = "Ubuntu Image"
+            vpcSubnetId = "subnet-0c23f411b0800b216"
+            keyPairName = "daria.krupkina"
+            instanceType = "t2.medium"
+            securityGroups = listOf("sg-072d8bfa0626ea2a6")
+            source = Source("ami-07908fe7a17542f6b")
+        }
         amazonEC2CloudProfile {
             id = "amazon-1"
             name = "Cloud Profile"
@@ -42,8 +53,8 @@ project {
                 keyId = "credentialsJSON:c4151395-a0a5-4db6-9697-918ebca829e5"
                 secretKey = "credentialsJSON:42f04976-3912-4b71-8161-3e9ca9484e7d"
             }
-            param("terminateTimeOut_checkbox", "false")
             param("terminate-after-build", "false")
+            param("terminateTimeOut_checkbox", "false")
         }
     }
 }
