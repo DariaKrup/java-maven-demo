0cf3030ec9fbc10dbbbb4bb05b937676955f8f13
dkrupkina <dariakrupkina@gmail.com>
2023-08-10 13:32:41 +0200

    Subproject configuration to different file (TW-83052)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,10 +2,7 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
-import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
-import vars.teamcity.usage.ProjectName
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -59,7 +56,7 @@ project {
         }
     }
 
-    subProject(BookingApiPayconiq)
+    subProject(Booking_settings.BookingApiPayconiq)
 }
 
 object Build : BuildType({
@@ -88,39 +85,4 @@ object Build : BuildType({
 })
 
 
-object BookingApiPayconiq : Project({
-    name = "BookingApiPayconiq"
 
-    val mainProjectName = "${ProjectName}_Build_1"
-    vcsRoot(BookingApiPayconiq_HttpsGithubComDariaKrupBookingApiPayconiqRefsHeadsMaster)
-
-    template(BookingApiPayconiq_BuildConfig)
-})
-
-object BookingApiPayconiq_BuildConfig : Template({
-    name = "Build_Config"
-    artifactRules = "**/*=>archive.zip"
-
-    vcs {
-        root(BookingApiPayconiq_HttpsGithubComDariaKrupBookingApiPayconiqRefsHeadsMaster)
-    }
-
-    steps {
-        script {
-            name = "Output"
-            id = "RUNNER_15"
-            scriptContent = "echo 'booking subproject'"
-        }
-    }
-})
-
-object BookingApiPayconiq_HttpsGithubComDariaKrupBookingApiPayconiqRefsHeadsMaster : GitVcsRoot({
-    name = "https://github.com/DariaKrup/BookingApiPayconiq#refs/heads/master"
-    url = "https://github.com/DariaKrup/BookingApiPayconiq"
-    branch = "refs/heads/master"
-    branchSpec = "refs/heads/*"
-    authMethod = password {
-        userName = "DariaKrup"
-        password = "credentialsJSON:63794333-2fd2-47da-8fd5-83b767905c78"
-    }
-})
